348fb96c64ea0bcf6f8c8035fbb81ddefaebd187
Eric Felts <echristianfelts@gmail.com>
2021-08-10 15:41:34 -0700

    Finished 9 - Player Controller.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    [Header("Stats")]
+    public float moveSpeed;
+    public float jumpForce;
+
+    [Header("Components")]
+    public Rigidbody rig;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,30 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            TryJump();
+    }
+
+    void Move()
+    {
+        // get the input axis
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        // calculate a direction relative to where we're facing
+        Vector3 dir = (transform.forward * z + transform.right * x) * moveSpeed;
+        dir.y = rig.velocity.y;
+        // set that as our velocity
+        rig.velocity = dir;
+    }
+
+    void TryJump()
+    {
+        // create a ray facing down
+        Ray ray = new Ray(transform.position, Vector3.down);
+        // shoot the raycast
+        if (Physics.Raycast(ray, 1.5f))
+            rig.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
 }
