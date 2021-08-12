6bf0eb00e1a2a877a68ecd5905d13f5ccfaca7ad
Eric Felts <echristianfelts@gmail.com>
2021-08-12 01:52:31 -0700

    Completed 17 - Lobby Browser.

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -64,6 +64,9 @@ public class Menu : MonoBehaviourPunCallbacks, ILobbyCallbacks
 
         // activate the requested screen
         screen.SetActive(true);
+
+        if (screen == lobbyBrowserScreen)
+            UpdateLobbyBrowserUI();
     }
 
     // called when the "Back" button gets pressed
@@ -144,10 +147,62 @@ public class Menu : MonoBehaviourPunCallbacks, ILobbyCallbacks
         NetworkManager.instance.photonView.RPC("ChangeScene", RpcTarget.All, "Game");
     }
 
+
+    // Called when the Leave Lobby Button is pressed.
     public void OnLeaveLobbyButton()
     {
         PhotonNetwork.LeaveRoom();
         SetScreen(mainScreen);
+    }
+
+    // LOBBY BROWSER SCREEN
+
+    GameObject CreateRoomButton()
+    {
+        GameObject buttonObj = Instantiate(roomButtonPrefab, roomListContainer.transform);
+        roomButtons.Add(buttonObj);
+        return buttonObj;
+    }
+
+    void UpdateLobbyBrowserUI()
+    {
+        // disable all room buttons
+        foreach (GameObject button in roomButtons)
+            button.SetActive(false);
+            // display all current rooms in the master server
+        for (int x = 0; x < roomList.Count; ++x)
+        {
+            //  Again:  I am totally lost and have no idea what is happening in here...!!!
+            // get or create the button object
+            GameObject button = x >= roomButtons.Count ? CreateRoomButton() : roomButtons[x];
+            button.SetActive(true);
+            // set the room name and player count texts
+            button.transform.Find("RoomNameText").GetComponent<TextMeshProUGUI>().text = roomList[x].Name;
+            button.transform.Find("PlayerCountText").GetComponent<TextMeshProUGUI>().text = roomList[x].PlayerCount + " / " + roomList[x].MaxPlayers;
+
+            // set the button OnClick event
+            Button buttonComp = button.GetComponent<Button>();
+            string roomName = roomList[x].Name;
+            buttonComp.onClick.RemoveAllListeners();
+            buttonComp.onClick.AddListener(() => { OnJoinRoomButton(roomName); });
+        }
+
+
+    }
+
+    public void OnJoinRoomButton(string roomName)
+    {
+        NetworkManager.instance.JoinRoom(roomName);
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> allRooms)
+    {
+        roomList = allRooms;
+    }
+
+    public void OnRefreshButton()
+    {
+        UpdateLobbyBrowserUI();
     }
 
 }
